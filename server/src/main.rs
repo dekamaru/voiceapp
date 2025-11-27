bb60767f3eecde62ba797bca0786aa9975ac804e
dekamaru <akitomiz@gmail.com>
2025-11-27 21:36:41 +0100

    fix networking

--- server/src/main.rs
+++ server/src/main.rs
@@ -24,14 +24,14 @@ async fn main() {
     let management_server = ManagementServer::new();
     let management_server_for_relay = management_server.clone();
     let management_thread = tokio::spawn(async move {
-        if let Err(e) = management_server.run("127.0.0.1:9001").await {
+        if let Err(e) = management_server.run("0.0.0.0:9001").await {
             error!("ManagementServer error: {}", e);
         }
     });
 
     let voice_relay_server = VoiceRelayServer::new(management_server_for_relay);
     let voice_relay_thread = tokio::spawn(async move {
-        if let Err(e) = voice_relay_server.run("127.0.0.1:9002").await {
+        if let Err(e) = voice_relay_server.run("0.0.0.0:9002").await {
             error!("VoiceRelayServer error: {}", e);
         }
     });
